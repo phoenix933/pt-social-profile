4eec41d5ba7595b26c298c092ee9dc74ba2c8b3a
crashdev226 <midashand44@gmail.com>
2016-03-02 07:34:56 +0600

    initial setup with widget class

---
+++ PT-social-profile.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: PT Social Profile
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A plugin for displaying social profile links on widget areas on any WordPress Theme
+Version: 1.0
+Author: Kamal Ahmed
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
